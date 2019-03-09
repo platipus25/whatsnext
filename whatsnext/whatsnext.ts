727eadc391c69143d2a1fe98780650021cee23f6
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-09 10:30:25 -0800

    create WhatsnextUpdating

--- whatsnext/whatsnext.ts
+++ whatsnext/whatsnext.ts
@@ -1,2 +1,30 @@
 import schedule_base from "./generator.js"
+import Time from "./time.js"
+import { NetworkInterfaceInfo } from "os";
 
+class Whatsnext {
+    schedule_base: Object;
+    date: Date
+    contructor(schedule_base, date: Date){
+        this.schedule_base = schedule_base
+        this.date = date
+    }
+
+    now(){
+        return this.date
+    }
+
+    time(){
+        return Time.fromDate(this.now())
+    }
+
+    
+}
+
+class WhatsnextUpdating extends Whatsnext {
+    now(){
+        return new Date()
+    }
+}
+
+export default Whatsnext;
