aa418e2837911321043374a55bf41583b5aa7e91
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-08 19:59:01 -0800

    export a promise

--- whatsnext/generator.ts
+++ whatsnext/generator.ts
@@ -1,13 +1,20 @@
-import Time from "./time"
+import Time from "./time.ts"
 import ky from "ky"
 
 
-let schedule_base = {};
+var request = ky("schedule2018-19.json").json()
 
-(async () => { 
-    var response = await ky("schedule2018-19.json").json()
-    console.log(response)
-})();
-console.log(Time.fromDate(new Date()))
+
+let schedule_base = (request.then((data) => {
+    for(let day in data){
+        for(let periodIndex in data[day]){
+            let period = data[day][periodIndex]
+            data[day][periodIndex].start = Time.fromTs(period.start)
+            data[day][periodIndex].end = Time.fromTs(period.end)
+        }
+    }
+    //console.log(data)
+    return data;
+}))
 
 export default schedule_base;
