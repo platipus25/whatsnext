4c8ce5cd43aae32bae63ece16498d61d686be97e
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-13 16:36:02 -0700

    Generate

--- whatsnext/generator.ts
+++ whatsnext/generator.ts
@@ -7,12 +7,14 @@ var request = ky("schedule2018-19.json").json()
 
 let schedule_base = (request.then((data) => {
     for(let day in data){
-        for(let periodIndex in data[day]){
-            let period = data[day][periodIndex]
-            data[day][periodIndex].start = Time.fromTs(period.start)
-            data[day][periodIndex].end = Time.fromTs(period.end)
-            data[day][periodIndex].class = {name: period.name}
+        for(let periodIndex in data[day].periods){
+            let period = data[day]["periods"][periodIndex]
+            data[day]["periods"][periodIndex].start = Time.fromTs(period.start)
+            data[day]["periods"][periodIndex].end = Time.fromTs(period.end)
+            data[day]["periods"][periodIndex].class = {name: period.name}
         }
+        data[day].start = Time.fromTs(data[day].start)
+        data[day].end = Time.fromTs(data[day].start)
     }
     //console.log(data)
     return data;
