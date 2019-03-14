9f0119d8dae5f05d5d922ef3bdb14013bf586a16
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-13 17:44:08 -0700

    Implement day and schedule

--- whatsnext/whatsnext.ts
+++ whatsnext/whatsnext.ts
@@ -1,4 +1,3 @@
-//import schedule_base from "./generator.ts"
 import Time from "./time.ts"
 
 class Whatsnext {
@@ -7,6 +6,7 @@ class Whatsnext {
     constructor(schedule_base: Object, date: Date){
         this.schedule_base = schedule_base
         this.date = date
+        console.log(this.day, this.schedule)
     }
 
     now(){
@@ -15,6 +15,32 @@ class Whatsnext {
 
     time(){
         return Time.fromDate(this.now())
+    }
+
+    private _day(){ 
+        return this.day.slice(0, 3)
+    }
+
+    get day(){
+        let days_of_the_week = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+        let day = ""
+        let intDay = this.now().getDay()
+        day = days_of_the_week[intDay]
+
+        // search this.schedule_base["minimum_days"] for this.now()
+        for(let entry of this.schedule_base["minimum_days"]){
+            if(entry.hasOwnProperty("date")){
+                if(entry.date.toDateString() == this.now().toDateString()){
+                    day = "minimum"
+                }
+            }
+        }
+
+        return day
+    }
+
+    get schedule(){
+        return this.schedule_base[this._day()]
     }
 }
 
