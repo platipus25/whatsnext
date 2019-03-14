36caecddebc9c1f0c3bb653f1ed74f439aea52bd
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-13 17:43:36 -0700

    Process minimum days

--- whatsnext/generator.ts
+++ whatsnext/generator.ts
@@ -7,6 +7,13 @@ var request = ky("schedule2018-19.json").json()
 
 let schedule_base = (request.then((data) => {
     for(let day in data){
+        if(Array.isArray(data[day]) && data[day][0].hasOwnProperty("date")){
+            for(let entry in data[day]){
+                let date = data[day][entry].date
+                data[day][entry].date = new Date(date.year, date.month, date.day)
+            }
+            continue;
+        } 
         for(let periodIndex in data[day].periods){
             let period = data[day]["periods"][periodIndex]
             data[day]["periods"][periodIndex].start = Time.fromTs(period.start)
