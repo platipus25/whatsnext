bac26d3cb36722eeb30760ba48fddfda0cfc9d2e
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-15 21:53:40 -0700

    UpdatingIsh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Time from "./whatsnext/time.ts"
 //import Period from "./whatsnext/period.js"
 import schedulePromise from "./whatsnext/generator.ts"
-import {Whatsnext, WhatsnextUpdating} from "./whatsnext/whatsnext.ts"
+import {Whatsnext, WhatsnextUpdating, WhatsnextUpdatingIsh} from "./whatsnext/whatsnext.ts"
 var now = Time.fromDate(new Date())
 var now2 = new Time(14, 25)
 console.log(now.toString())
@@ -9,13 +9,19 @@ console.log(now2.toString())
 
 schedulePromise.then((schedule_base) => {
     var whatsnext = new Whatsnext(schedule_base, new Date(2018, 9, 5, 9, 41))
-    //whatsnext = new WhatsnextUpdating(schedule_base)
-    console.log(whatsnext,
+    whatsnext = new WhatsnextUpdatingIsh(schedule_base, new Date(2018, 9, 5, 7, 0), 1)
+    var cancel = 0
+    setInterval(() => {
+        clearInterval(cancel)
+        console.log(whatsnext,
                 whatsnext.day,
                 whatsnext.schedule,
-                whatsnext.thisClassCountdown((ts) => { 
-                    document.write(`${ts.toString()}</br>`)
+                cancel = whatsnext.thisClassCountdown((ts) => { 
+                    console.log(`${ts}`)
                 }),
                 whatsnext.thisClass(),
-                whatsnext.nextClass())
+                whatsnext.nextClass(),
+                whatsnext.now()
+                )
+            }, 1000)
 })
