4105e9952baa03f7b611b5a4da19ccc541cfb766
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-17 14:47:23 -0700

    stuff

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ import data from "schedule2018-19.json"
 
 schedulePromise.then((schedule_base) => {
     var whatsnext = new Whatsnext(schedule_base, new Date(2018, 9, 5, 9, 41))
-    whatsnext = new WhatsnextUpdatingIsh(schedule_base, new Date(2018, 9, 5, 7, 0), 1)
+    whatsnext = new WhatsnextUpdatingIsh(schedule_base, new Date(2018, 9, 5, 7, 0), 1.5)
     //whatsnext = new WhatsnextUpdating(schedule_base)
     var cancel = 0
-    whatsnext.endOfSchoolCountdown((ts) => console.log(ts.toString()))
+    let printts = (ts) => console.log(ts.toString())
+    whatsnext.endOfSchoolCountdown((ts) => {
+        document.getElementById("endOfSchoolCountdown").innerHTML = ts.toHTML()
+    })
+    whatsnext.thisClassCountdown((ts) => {
+        document.getElementById("thisClassCountdown").innerHTML = ts.toHTML()
+    })
+    whatsnext.nextClassCountdown((ts) => {
+        document.getElementById("nextClassCountdown").innerHTML = ts.toHTML()
+    })
+
     setInterval(() => {
         clearInterval(cancel)
-        
+        document.getElementById("nextClass").innerText = (whatsnext.nextClass() || {name:""}).name
+        document.getElementById("thisClass").innerText = (whatsnext.thisClass() || {name:""}).name
         console.log(
                 whatsnext.day,
                 whatsnext.thisClass(),
