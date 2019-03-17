f96761f0775294442854b689406dabfb3a29a8c6
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-03-17 13:59:53 -0700

    stuff

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ import data from "schedule2018-19.json"
 schedulePromise.then((schedule_base) => {
     var whatsnext = new Whatsnext(schedule_base, new Date(2018, 9, 5, 9, 41))
     whatsnext = new WhatsnextUpdatingIsh(schedule_base, new Date(2018, 9, 5, 7, 0), 1)
+    //whatsnext = new WhatsnextUpdating(schedule_base)
     var cancel = 0
+    whatsnext.endOfSchoolCountdown((ts) => console.log(ts.toString()))
     setInterval(() => {
         clearInterval(cancel)
+        
         console.log(
                 whatsnext.day,
                 whatsnext.thisClass(),
