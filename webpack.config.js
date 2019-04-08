fd693363d820ed428c641e6258c1f275a789086b
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-04-08 15:51:48 -0700

    Split targets in config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,72 @@
 const path = require('path');
 
+let testConfig = {
+  entry: './index.js',
+  mode: "development",
+  output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "src")
+    ]
+  }
+};
+
+let webConfig = {
+  target: "web",
+  entry: './src/whatsnext.ts',
+  mode: "production",
+  output: {
+      filename: 'whatsnext.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "src")
+    ]
+  }
+};
+
+let nodeConfig = {
+  target:"node",
+  entry: './src/whatsnext.ts',
+  mode: "production",
+  output: {
+      filename: 'whatsnext.node.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "src")
+    ]
+  }
+};
+
 module.exports = {
     entry: './src/whatsnext.ts',
     mode: "production",
