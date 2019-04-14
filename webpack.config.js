192ed7ef73ab4e4c3930a2965cd05a7850a410e3
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-04-14 16:52:11 -0700

    change build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,13 @@
 const path = require('path');
 
-let testConfig = {
-  entry: './index.js',
-  mode: "development",
-  output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader' }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 8080
-  },
-  resolve: {
-    modules: [
-      "node_modules",
-      path.resolve(__dirname, "src")
-    ]
-  }
-};
-
-let webConfig = {
-  target: "web",
+let baseConfig = {
   entry: './src/whatsnext.ts',
   mode: "production",
   output: {
       filename: 'whatsnext.js',
-      path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader' }
-    ]
-  },
-  resolve: {
-    modules: [
-      "node_modules",
-      path.resolve(__dirname, "src")
-    ]
-  }
-};
-
-let nodeConfig = {
-  target:"node",
-  entry: './src/whatsnext.ts',
-  mode: "production",
-  output: {
-      filename: 'whatsnext.node.js',
       path: path.resolve(__dirname, 'dist'),
       library: "whatsnext",
-      libraryTarget: "umd"
+      libraryTarget: "ES2015"
   },
   module: {
     rules: [
@@ -67,6 +20,23 @@ let nodeConfig = {
       path.resolve(__dirname, "src")
     ]
   }
-};
+}
 
-module.exports = [testConfig, webConfig, nodeConfig]
+let webConfig = {
+  ...baseConfig, 
+  target: "web",
+  output:{
+    filename: "whatsnext.web.js"
+  }
+}
+
+
+let nodeConfig = {
+  ...baseConfig, 
+  target: "node",
+  output:{
+    filename: "whatsnext.node.js"
+  }
+}
+
+module.exports = [webConfig, nodeConfig]
