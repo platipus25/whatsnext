6b0ab94251043f4f645201e86c3f05143fc91489
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-05-14 16:41:21 -0700

    update examples

--- examples/web/index.js
+++ examples/web/index.js
@@ -1,8 +1,7 @@
-
 (async () => {
     let schedule_base = await window.ky.default.get("/examples/config_files/schedule2018-19.json").json()
-    let instance = new whatsnext.Whatsnext(schedule_base)
-    window.instance = instance
+    let inst = new whatsnext.Whatsnext(schedule_base)
+    window.instance = inst
     window.schedule_base = schedule_base
     setInterval(() => {
         $("#thisClassCountdown").text(instance.thisClassCountdown())
@@ -15,3 +14,16 @@
     }, 1000)
     console.log(schedule_base)
 })()
+
+$("#whatsnext_form").on("submit",function(event){
+    event.preventDefault()
+    console.log(event)
+    let interval = $("#interval").val()
+    let date = new Date(Date.parse($("#date").val()))
+    console.log(interval, date)
+    window.instance = new whatsnext.WhatsnextSim(window.schedule_base, interval, date)
+    //updateFormDate()
+})
+
+window.updateFormDate = () => $("#date").val((new Date()).toLocaleString())
+updateFormDate()
