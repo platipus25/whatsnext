5bf32c9f652ef07c2fc6f53723d7cba92e30639a
platipus25 <40722163+platipus25@users.noreply.github.com>
2020-01-11 19:51:09 -0800

    Fix weird optimization bug It was skipping a continue so replaced with if statement

--- src/generator.ts
+++ src/generator.ts
@@ -1,30 +1,31 @@
-import Time from "../node_modules/time_ts/dist/time"
-import Period from "./period.ts"
+import Time from "./Time"
+import Period from "./Period"
 
 function transformFromRaw(object: any){
     for(let nodeIndex in object){
         let node = object[nodeIndex]
-        if(!node) continue;
+        if(node != null) {
 
-        let prototype = Object.getPrototypeOf(node)
-        let isPlainObject = prototype == Object.prototype || prototype == Array.prototype
-        let isTsTime = node.hasOwnProperty("hour") && node.hasOwnProperty("minute") && isPlainObject
-        let isTsDate = node.hasOwnProperty("year") && node.hasOwnProperty("month") && node.hasOwnProperty("day") && isPlainObject
-        let isPeriod = node.hasOwnProperty("start") && node.hasOwnProperty("end") && node.hasOwnProperty("name") && isPlainObject
-        let shouldIterateOn = typeof node == "object" && node != null && isPlainObject;
-        
-        // if this instance can't fufill the change
-        if(shouldIterateOn && !isTsTime && !isTsDate){ // checking if this instance can fulfill so as not to make extras
-            object[nodeIndex] = transformFromRaw(node);
-        }
+            let prototype = Object.getPrototypeOf(node)
+            let isPlainObject = prototype == Object.prototype || prototype == Array.prototype
+            let isTsTime = node.hasOwnProperty("hour") && node.hasOwnProperty("minute") && isPlainObject
+            let isTsDate = node.hasOwnProperty("year") && node.hasOwnProperty("month") && node.hasOwnProperty("day") && isPlainObject
+            let isPeriod = node.hasOwnProperty("start") && node.hasOwnProperty("end") && node.hasOwnProperty("name") && isPlainObject
+            let shouldIterateOn = typeof node == "object" && node != null && isPlainObject;
+            
+            // if this instance can't fufill the change
+            if(shouldIterateOn && !isTsTime && !isTsDate){ // checking if this instance can fulfill so as not to make extras
+                object[nodeIndex] = transformFromRaw(node);
+            }
 
-        // at the end of the tree
-        if(isTsTime){
-            object[nodeIndex] = Time.fromTs(node)
-        }else if(isTsDate){
-            object[nodeIndex] = new Date(node.year, node.month, node.day)
-        }else if(isPeriod){
-            object[nodeIndex] = new Period(node)
+            // at the end of the tree
+            if(isTsTime){
+                object[nodeIndex] = Time.fromTs(node)
+            }else if(isTsDate){
+                object[nodeIndex] = new Date(node.year, node.month, node.day)
+            }else if(isPeriod){
+                object[nodeIndex] = new Period(node)
+            }
         }
     }
     return object
