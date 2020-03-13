5322ca247bf712eab1ec003f976202f1206db412
platipus25 <40722163+platipus25@users.noreply.github.com>
2020-03-12 23:11:33 -0700

    Add constructor overload

--- src/period.ts
+++ src/period.ts
@@ -1,19 +1,24 @@
 import Time from "./time"
+import { parse } from "./generator"
 
 class Period { 
     readonly name: string;
     readonly start: Time;
     readonly end: Time;
-    constructor(name: string, start: Time, end: Time){
+
+    constructor(name: string, start: string, end: string)
+    constructor(name: string, start: Time, end: Time)
+    
+    constructor(name: string, start: Time | string, end: Time | string) {
         this.name = name
-        this.start = start
-        this.end = end
+        this.start = start instanceof Time? start: parse(start)
+        this.end = end instanceof Time? end: parse(end)
     }
 
     percent(now: Date = new Date()): number | null {
-        let lengthMs = this.end.toDate(now).valueOf() - this.start.toDate(now).valueOf()
-        let timePass = now.valueOf() - this.start.toDate(now).valueOf()
-        let percent = (timePass / lengthMs) * 100
+        const lengthMs = this.end.toMs() - this.start.toMs()
+        const timePass = now.valueOf() - this.start.toMs()
+        const percent = (timePass / lengthMs) * 100
 
         return percent
     }
