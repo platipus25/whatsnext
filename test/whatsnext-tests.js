d14d092cfdff1b61653bed8b16466f9bbd5e6f21
platipus25 <40722163+platipus25@users.noreply.github.com>
2020-03-15 21:45:45 -0700

    Try to set up testing

---
+++ test/whatsnext-tests.js
@@ -0,0 +1,17 @@
+/*const test = require('ava');
+const whatsnext = require("../public/whatsnext.js")
+
+// Whatsnext
+const { Whatsnext } = whatsnext
+
+//TODO: Should test modifying schedule_base 
+test('modifying schedule_base', t => {
+	let schedule_base = {
+		"A field": [
+			{ name: "A period", start: "12:30", end: "15:00" }
+		]
+	}
+	const inst = new Whatsnext(schedule_base)
+
+	//t.pass()
+})*/
