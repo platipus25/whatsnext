dd9aeed027961622380c6f4e9084ae0003ea15c4
platipus25 <40722163+platipus25@users.noreply.github.com>
2020-03-15 21:49:11 -0700

    Change for new locations

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,11 +1,21 @@
-let { Whatsnext, Time } = require("../../dist/whatsnext.node.js")
-let schedule_base = require("../config_files/schedule2018-19.json")
+let { Whatsnext, Time } = require("../../public/whatsnext.js")
+let m = require("../..")
+let schedule_base = require("../config_files/MVHS2019-20_new.json")
 
 let inst = new Whatsnext(schedule_base)
+const config_id = "B"
+
+let cfg = inst.schedule_base.getTimeline(config_id)
+
 console.log(
-    inst.now,
-    inst.thisClass(),
-    inst.enumerateNextClass(),
-    inst.setTimeDate({date: Time.fromDate(new Date())}, new Date()),
-    Time.fromDate(new Date())
+    inst,
+    cfg
+)
+
+schedule_base[config_id][0] = {name:"A totally different name", start:"2:30", end:"4:5"}
+
+console.log(inst.schedule_base.getTimeline(config_id), cfg)
+
+console.log(
+    Object.isFrozen(schedule_base)
 )
